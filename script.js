a8f6ea80d44d33c46371b5dfa591660488e5ba36
Utkarsh Choudhary <72205579+UtkarshChoudhary19@users.noreply.github.com>
2023-06-28 18:26:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function threeSum(arr, target) {
 // write your code here
-  
+let binary = decimal.toString( redix );
+
+
+Let binary = (decimal >>> 0).toString( redix );  
 }
 
 module.exports = threeSum;
