17a1c9b3c25a9139b6310132007bdc8dc62c65c1
Utkarsh Choudhary <72205579+UtkarshChoudhary19@users.noreply.github.com>
2023-07-02 16:29:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
-function threeSum(arr, target) {
-// write your code here
-let binary = decimal.toString( redix );
 
-
-let binary = (decimal >>> 0).toString( redix );  
+// JavaScript program to implement the approach
+// function to convert decimal to binary
+function DecimalToBinary(num)
+{
+	var str = "";
+	while (num)
+	{
+	
+		// adding the result of num & 1 to str
+		if (num & 1) // 1
+			str += '1';
+		else // 0
+			str += '0';
+			
+		// Right Shift by 1
+		num >>= 1;
+	}
+	return str;
 }
 
-module.exports = threeSum;
+// function to print reverse of str
+function reverse(str)
+{
+	var res = "";
+	for (var i = str.length - 1; i >= 0; i--)
+		res += (str[i]);
+	document.write(res);
+}
+
+// Driver Code
+var num = 59;
+document.write("Binary of num " + num + " is: ");
+
+// function call
+reverse(DecimalToBinary(num));
+
